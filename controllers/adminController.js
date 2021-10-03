396163da70cf9a0abcc65e0a1755964d372fb019
YasinVeliyev <veliyev.yasin@gmail.com>
2021-10-03 22:57:46 +0400

    modified:   .prettierrc 	modified:   controllers/adminController.js 	modified:   controllers/authController.js 	modified:   controllers/shopController.js 	modified:   views/admin/edit-product.ejs

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,8 @@
-const Product = require("../models/json/productModel")
-const ProductMongo = require("../models/mongodb/productMongoModel")
-const ProductMongooseModel = require("../models/mongoose/productMongooseModel")
-const ProductMysqlModel = require("../models/mysql/productMysqlModel")
-const productSequelizeModel = require("../models/mysql/productSequelizeModel")
+const Product = require("../models/json/productModel");
+const ProductMongo = require("../models/mongodb/productMongoModel");
+const ProductMongooseModel = require("../models/mongoose/productMongooseModel");
+const ProductMysqlModel = require("../models/mysql/productMysqlModel");
+const productSequelizeModel = require("../models/mysql/productSequelizeModel");
 
 exports.getAddProduct = (req, res, next) => {
     res.render("admin/edit-product", {
@@ -12,11 +12,11 @@ exports.getAddProduct = (req, res, next) => {
         productCSS: true,
         activeAddProduct: true,
         editMode: false,
-    })
-}
+    });
+};
 
 exports.postAddProduct = (req, res, next) => {
-    const { title, price, imageUrl, description } = req.body
+    const { title, price, imageUrl, description } = req.body;
     productSequelizeModel
         .create({
             title,
@@ -25,22 +25,22 @@ exports.postAddProduct = (req, res, next) => {
             description,
         })
         .then(res.redirect("/products"))
-        .catch((err) => console.error(err))
+        .catch(err => console.error(err));
     // const product = new ProductMysqlModel(title, imageUrl, description, price);
     // product
     //     .save()
     //     .then(res.redirect("/products"))
     //     .catch((err) => console.log(err));
-}
+};
 
 exports.getEditProduct = async (req, res, next) => {
-    const editMode = req.query.editMode || false
-    const product = await productSequelizeModel.findByPk(req.params.productId)
+    const editMode = req.query.editMode || false;
+    const product = await productSequelizeModel.findByPk(req.params.productId);
     if (!product) {
         return res.status(404).render("404", {
             pageTitle: "Page Not Found",
             path: "404",
-        })
+        });
     }
     res.render("admin/edit-product", {
         pageTitle: "Edit Product",
@@ -50,13 +50,13 @@ exports.getEditProduct = async (req, res, next) => {
         activeAddProduct: true,
         editMode,
         product,
-    })
-}
+    });
+};
 
 exports.getProducts = async (req, res, next) => {
     productSequelizeModel
         .findAll()
-        .then((products) => {
+        .then(products => {
             return res.render("admin/products", {
                 prods: products,
                 pageTitle: "Admin Products",
@@ -64,21 +64,21 @@ exports.getProducts = async (req, res, next) => {
                 hasProducts: products.length > 0,
                 activeShop: true,
                 productCSS: true,
-            })
+            });
         })
-        .catch((err) => console.error(err))
-}
+        .catch(err => console.error(err));
+};
 
 exports.postEditProduct = (req, res, next) => {
-    const { id, title, price, imageUrl, description } = req.body
+    const { id, title, price, imageUrl, description } = req.body;
     productSequelizeModel
         .update({ title, price, imageUrl, description }, { where: { id } })
-        .then((result) => res.redirect("/admin/products", { isAuthenticated: req.isLoggedIn }))
-        .catch((err) => console.error(err))
-}
+        .then(result => res.redirect("/admin/products", { isAuthenticated: req.isLoggedIn }))
+        .catch(err => console.error(err));
+};
 
 exports.postDeleteProduct = async (req, res, next) => {
-    await productSequelizeModel.destroy({ where: { id: req.body.id } })
+    await productSequelizeModel.destroy({ where: { id: req.body.id } });
 
-    res.redirect("/admin/products", { isAuthenticated: req.isLoggedIn })
-}
+    res.redirect("/admin/products", { isAuthenticated: req.isLoggedIn });
+};
