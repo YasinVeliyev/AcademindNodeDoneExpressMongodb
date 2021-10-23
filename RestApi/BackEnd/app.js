b5f2dfc894a49cdf77bcb794f426d1d2e88e88cf
YasinVeliyev <veliyev.yasin@gmail.com>
2021-10-23 23:48:31 +0400

    modified:   app.js 	new file:   controllers/authController.js 	new file:   models/userModel.js 	modified:   package-lock.json 	modified:   package.json 	new file:   routes/auth.js 	modified:   ../FrondEnd/src/App.js

--- RestApi/BackEnd/app.js
+++ RestApi/BackEnd/app.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const path = require("path");
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 const fileStorage = multer.diskStorage({
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
---
+++ RestApi/BackEnd/routes/auth.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const { body } = require("express-validator");
+
+const User = require("../models/userModel");
+const authController = require("../controllers/authController");
+
+const router = Router();
+
+let validator = [
+    body("name").trim(),
+    body("email")
+        .trim()
+        .isEmail()
+        .withMessage("Please enter a valid email.")
+        .custom(async (value, { req }) => {
+            return (await User.findOne({ email: value })) || false;
+        })
+        .withMessage("E-mail already in use"),
+    body("password").isLength({ min: 6 }),
+];
+
+router.put("/signup", validator, authController.signup);
+router.post("/login", authController.login);
+
+module.exports = router;
